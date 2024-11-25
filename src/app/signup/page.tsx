6055e751ce5ff7c74adaf74149ee07b37b5c3307
Nihal Abdul Khader MP <84581207+Nihalmp45@users.noreply.github.com>
2024-11-25 09:15:26 +0530

    button disabled

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { axios } from "axios";
 
@@ -10,15 +10,26 @@ export default function SignupPage() {
     password: "",
     username: "",
   });
+  const [buttonDisabled,setButtonDisabled] = useState(false)
+
   const onSignup = async () => {};
 
+  useEffect(() => {
+    if (user.email.length >0 && user.password.length>0 && user.username.length>0){
+      setButtonDisabled(false)
+    }else(
+    setButtonDisabled(true))
+   
+  }, [user])
+  
+  
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Signup</h1>
       <hr />
       <label htmlFor="username">username</label>
       <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
         type="text"
         id="username"
         name="username"
@@ -29,7 +40,7 @@ export default function SignupPage() {
 
       <label htmlFor="email">email</label>
       <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
         type="email"
         id="email"
         name="email"
@@ -40,7 +51,7 @@ export default function SignupPage() {
 
       <label htmlFor="password">password</label>
       <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
         type="text"
         id="password"
         name="password"
@@ -48,7 +59,7 @@ export default function SignupPage() {
         onChange={(e) => setUser({ ...user, password: e.target.value })}
         placeholder="password"
       />
-      <button onSubmit={onSignup} className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">Sign up here</button>
+      <button onSubmit={onSignup} className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">{buttonDisabled ? "cant sign up now":"signup"}</button>
       <Link href='/login'>Visit login page</Link>
 
     </div>
